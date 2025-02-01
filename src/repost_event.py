a9ca741e7b2a6be7303ac4710acc450bd46b4f04
jared <git@sillock.io>
2025-02-01 12:57:32 +0000

    fix: image serialisation for rabbitmq messages

--- src/repost_event.py
+++ src/repost_event.py
@@ -2,19 +2,33 @@
 
 from typing import List
 from pydantic import BaseModel
+from pydantic import ConfigDict
+from pydantic.alias_generators import to_camel
 
 
 class Link(BaseModel):
     """Constructor used by pydantic"""
 
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True,
+        from_attributes=True,
+    )
     guild_id: str
     channel_id: str
     message_id: str
+    score: float
 
 
 class RepostEvent(BaseModel):
     """Constructor used by pydantic"""
 
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True,
+        from_attributes=True,
+    )
+    guild_id: str
     channel_id: str
     reply_image: bytes
     links: List[Link]
