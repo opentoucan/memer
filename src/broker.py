48782b6ee147f8c0e7e9faf48212715788093f7a
jared <git@sillock.io>
2025-02-05 15:01:02 +0000

    fix: use url env var for rabbitmq and qdrant with api key

--- src/broker.py
+++ src/broker.py
@@ -1,12 +1,6 @@
 import os
-from faststream.security import SASLPlaintext
 from faststream.rabbit import RabbitBroker
 
 broker = RabbitBroker(
-    host=os.environ.get("RABBITMQ_HOST", "localhost"),
-    port=int(os.environ.get("RABBITMQ_PORT", 5672)),
-    security=SASLPlaintext(
-        username=os.environ.get("RABBITMQ_USER", "guest"),
-        password=os.environ.get("RABBITMQ_PASSWORD", "guest"),
-    ),
+    url=os.environ.get("RABBITMQ_URL", "amqp://guest:guest@localhost:5672/")
 )
