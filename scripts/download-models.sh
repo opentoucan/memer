72b77a2f541c6be8ce58bb9da91630a3e789c994
jared <git@sillock.io>
2025-02-24 15:39:38 +0000

    ci: pass model at docker build time

--- scripts/download-models.sh
+++ scripts/download-models.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-curl -sL https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt \
-    --create-dirs \
-    -o ./resources/models/ViT-B-32.pt
+model=$0
+
+AVAILABLE_MODELS=$(curl -sL https://raw.githubusercontent.com/openai/CLIP/refs/heads/main/clip/clip.py | sed -n '/https:\/\/openaipublic.azureedge.net/p' | tr -d '\n')
+filename=$(echo $model | tr / -)
+echo "{${AVAILABLE_MODELS%,*}}" | jq --arg m "$model" -r '.[$m]' | xargs -I n1 curl -SL n1 --create-dirs -o ./resources/models/$filename.pt
