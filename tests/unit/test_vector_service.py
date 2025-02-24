d1b11e095ab8a8b247a97662aca6e37955cf5de5
sillock <mail@sillock.com>
2025-02-24 22:50:17 +0000

    test: add more test cases

--- tests/unit/test_vector_service.py
+++ tests/unit/test_vector_service.py
@@ -35,23 +35,32 @@ def test_different_memes_are_not_similar():
     # Arrange
     image_1 = Image.open("./tests/data/monkey_meme_1.jpg")
     image_2 = Image.open("./tests/data/monkey_meme_2.jpg")
+    image_3 = Image.open("./tests/data/boomer.jpg")
+    image_4 = Image.open("./tests/data/hotdog.png")
     vectors_1 = clip_service.get_vectors(image_1)
     vectors_2 = clip_service.get_vectors(image_2)
+    vectors_3 = clip_service.get_vectors(image_3)
+    vectors_4 = clip_service.get_vectors(image_4)
 
-    meme_1 = Meme(
+    # Act
+    vector_service.upload_vectors(vectors_2, Meme(
         sender="Mr Test",
         timestamp=dt.now().strftime("%m/%d/%Y %I:%M:%S %p"),
         guild_id="123",
         channel_id="123",
         message_id="123",
-    )
-
-    # Act
-    vector_service.upload_vectors(vectors_1, meme_1)
-    response = vector_service.query_vectors(vectors_2[0])
+    ))
+    vector_service.upload_vectors(vectors_3, Meme(
+        sender="Mr Test",
+        timestamp=dt.now().strftime("%m/%d/%Y %I:%M:%S %p"),
+        guild_id="123",
+        channel_id="123",
+        message_id="123",
+    ))
 
     # Assert
-    assert response.points == []
+    assert vector_service.query_vectors(vectors_1[0]).points == []
+    assert vector_service.query_vectors(vectors_4[0]).points == []
 
 def test_similar_memes_are_returned_a_score():
     """Tests similar memes are flagged as similar"""
