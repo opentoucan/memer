14d7d0029b21eec029dbbba091c8278cb782bd0f
sillock <mail@sillock.com>
2025-02-25 20:31:22 +0000

    fix: check if tools are installed before exiting in download script

--- scripts/download-model.sh
+++ scripts/download-model.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+if ! command -v yq 2>&1 >/dev/null
+then
+    echo "yq could not be found"
+    exit 1
+fi
+
+if ! command -v jq 2>&1 >/dev/null
+then
+    echo "jq could not be found"
+    exit 1
+fi
+
 model=$(yq -oy '.app.model.name' pyproject.toml)
 
 AVAILABLE_MODELS=$(curl -sL https://raw.githubusercontent.com/openai/CLIP/refs/heads/main/clip/clip.py | sed -n '/https:\/\/openaipublic.azureedge.net/p' | tr -d '\n')
